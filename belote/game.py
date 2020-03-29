e6869e85a0c03e59f97331106721d57a3d4d93d4
Florian Denis <me@floriandenis.net>
2020-03-29 18:06:01 +0200

    Move “taking” and scoring logic outside of game into pli

--- belote/game.py
+++ belote/game.py
@@ -13,6 +13,7 @@ from enum import Enum
 from . import card
 from . import constants
 from . import player
+from . import pli
 
 
 log = logging.getLogger(__name__)
@@ -39,12 +40,11 @@ class Game:
         # Map player -> array of card in hand
         self._hands = {}
 
-        # Map player -> card played (currently on the table)
-        self._current_pli = {}
+        # Who started the current round (a "round" is 8 pli)
+        self._starting_player = 0
 
-        # Who started the current round, and the current pli
-        self._starting_player_round = 0
-        self._starting_player_pli = 0
+        # The current pli being played
+        self._current_pli = None
 
         # By team (even/odd), won pli so far and point total
         self._plis = [[], []]
@@ -81,7 +81,7 @@ class Game:
     def _deal(self, deck):
 
         ordered_players = [self._players[idx % 4] for idx in range(
-            self._starting_player_round, self._starting_player_round + 4)]
+            self._starting_player, self._starting_player + 4)]
 
         idx = 0
         dealt = {}
@@ -104,6 +104,12 @@ class Game:
         return dealt
 
 
+    def _start_pli(self, starting_player):
+        self._current_pli = pli.Pli(self._players, starting_player)
+
+        self.on_status_changed()
+
+
     def _start_round(self):
         if self.state != Game.State.READY_TO_START:
             log.error("Invalid state to start new round")
@@ -114,7 +120,7 @@ class Game:
         self._deck = self._cut(self._deck)
         self._hands = self._deal(self._deck)
 
-        self.on_status_changed()
+        self._start_pli(self._starting_player)
 
 
     def set_player_ready(self, player):
@@ -146,7 +152,7 @@ class Game:
             log.error("Can only pick the trump suit while announcing")
             return
 
-        if not player is self._players[self._starting_player_round]:
+        if not player is self._players[self._starting_player]:
             log.error("Only the starting player shall select the trump suit")
             return
 
@@ -159,31 +165,28 @@ class Game:
             log.error("Invalid state to play card")
             return
 
-        current_player_idx = (
-            self._starting_player_pli + len(self._current_pli)) % 4
-        if player is not self._players[current_player_idx]:
-            log.error("Not player's turn")
-            return
-
         hand = self._hands[player]
         if not card in hand:
             log.error("Trying to play card not in player's hand")
             return
 
-        if player in self._current_pli:
-            log.error("Player already played")
+        if not self._current_pli.can_play_card(player, card, hand):
+            log.error("Given player cannot play this card right now")
             return
 
+        # Actually play the card
+        self._current_pli.play_card(player, card)
         self._hands[player].remove(card)
-        self._current_pli[player] = card
 
-        if len(self._current_pli) == 4 and len(self._hands[player]) == 0:
-            # Round completed, will terminate the round in 2 seconds
-            threading.Timer(2, self._finish_round).start()
+        is_last_pli = len(self._hands[player]) == 0
 
-        elif len(self._current_pli) == 4:
-            # Hand completed, will reset the pli in 2 seconds
-            threading.Timer(2, self._finish_pli).start()
+        if self._current_pli.is_complete:
+            if is_last_pli:
+                # Round completed, will terminate the round in 2 seconds
+                threading.Timer(2, self._finish_round).start()
+            else:
+                # Hand completed, will reset the pli in 2 seconds
+                threading.Timer(2, self._finish_pli).start()
 
         self.on_status_changed()
 
@@ -191,42 +194,25 @@ class Game:
     def _finish_pli(self):
 
         # Find out who won it
-        ordered_players_idx = [
-            (self._starting_player_pli + 0) % 4,
-            (self._starting_player_pli + 1) % 4,
-            (self._starting_player_pli + 2) % 4,
-            (self._starting_player_pli + 3) % 4,
-        ]
+        taking_player = self._current_pli.taking_player(self._trump_suit)
+        taking_player_idx = self._players.index(taking_player)
 
-        overtaking_card = None
-        overtaking_player_idx = None
-        for idx in ordered_players_idx:
-            played_card = self._current_pli[self._players[idx]]
-            if played_card.overtakes(overtaking_card, self._trump_suit):
-                overtaking_card = played_card
-                overtaking_player_idx = idx
+        is_last_pli = len(self._hands[taking_player]) == 0
 
-        overtaking_player = self._players[overtaking_player_idx]
-        is_last_pli = len(self._hands[overtaking_player]) == 0
+        points = (self._current_pli.total_points(self._trump_suit)
+            + (10 if is_last_pli else 0))
 
-        pli = self._current_pli.values()
-        points = sum([card.point_value(self._trump_suit) for card in pli]) + (
-            10 if is_last_pli else 0)
+        self._plis[taking_player_idx % 2].append(self._current_pli)
+        self._points[taking_player_idx % 2] += points
 
-        self._plis[overtaking_player_idx % 2] += pli
-        self._points[overtaking_player_idx % 2] += points
-        self._current_pli = {}
-        self._starting_player_pli = overtaking_player_idx
-
-        self.on_status_changed()
+        self._start_pli(taking_player_idx)
 
 
     def _reset_round(self):
-        self._current_pli = {}
+        self._current_pli = None
         self._hands = {}
         self._plis = [[], []]
-        self._starting_player_round = (self._starting_player_round + 1) % 4
-        self._starting_player_pli = self._starting_player_round
+        self._starting_player = (self._starting_player + 1) % 4
         self._round_ongoing = False
         self._trump_suit = None
 
@@ -242,7 +228,9 @@ class Game:
         self._finish_pli()
 
         # Reasssemble the deck
-        self._deck = self._plis[0] + self._plis[1]
+        odd_cards = sum([pli.cards for pli in self._plis[0]], [])
+        even_cards = sum([pli.cards for pli in self._plis[1]], [])
+        self._deck = odd_cards + even_cards
 
         # Clean current round
         self._reset_round()
@@ -287,7 +275,6 @@ class Game:
             (player_index + 3) % 4
         ]
 
-
         proxy = GameProxy()
 
         proxy._state = self.state
@@ -295,7 +282,13 @@ class Game:
         proxy._player_points = round(self._points[(player_index % 2)])
         proxy._enemy_points  = round(self._points[(player_index + 1) % 2])
 
-        proxy._starting_player = idx_permutation.index(self._starting_player_pli)
+
+        starting_player_idx = (self._players.index(
+            self._current_pli.starting_player)
+                if self._current_pli
+            else self._starting_player)
+
+        proxy._starting_player = idx_permutation.index(starting_player_idx)
 
         proxy._players = [
             self._players[idx].name if idx < len(self._players) else ""
@@ -303,10 +296,11 @@ class Game:
         ]
 
         proxy._current_pli = [
-            self._current_pli[self._players[idx]]
-                if idx < len(self._players) and self._players[idx] in self._current_pli
-                    else card.Card("")
-                for idx in idx_permutation
+            self._current_pli.card_played_by(self._players[idx])
+                if self._current_pli and self._current_pli.card_played_by(
+                    self._players[idx])
+                else card.Card("")
+            for idx in idx_permutation
         ]
 
         proxy._hand = self._hands[player] if player in self._hands else []
---
+++ belote/pli.py
@@ -0,0 +1,94 @@
+#
+# Copyright (C) Florian Denis - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+#
+
+from . import card
+from . import player
+
+"""
+Pli abstraction
+Contains the context and logic for:
+ - Which card has been played in the current pli
+ - Can a player play a certain card ?
+ - Who is taking the pli so far ?
+"""
+class Pli:
+
+    def __init__(self, players, starting_player_idx):
+
+        # The array of all players around the table
+        self._players = players
+
+        # The index of the player that starts this pli
+        self._starting_player_idx = starting_player_idx
+
+        # A map of each card played
+        self._cards = {}
+
+
+    @property
+    def _ordered_player_indices(self):
+        return [
+            (self._starting_player_idx + 0) % 4,
+            (self._starting_player_idx + 1) % 4,
+            (self._starting_player_idx + 2) % 4,
+            (self._starting_player_idx + 3) % 4,
+        ]
+
+
+    @property
+    def cards(self):
+        # Return the array of cards played, in their played order
+        return [self._cards[self._players[idx]]
+            for idx in self._ordered_player_indices]
+
+
+    @property
+    def starting_player(self):
+        return self._players[self._starting_player_idx]
+
+
+    @property
+    def is_complete(self):
+        return len(self._cards) == 4
+
+
+    def card_played_by(self, player):
+        return self._cards[player] if player in self._cards  else None
+
+
+    def can_play_card(self, player, card, current_hand):
+        current_player_idx = self._ordered_player_indices[len(self._cards)]
+
+        # Complete ?
+        if self.is_complete:
+            return False
+
+        # This player's turn ?
+        if player is not self._players[current_player_idx]:
+            return False
+
+        return True
+
+
+    def play_card(self, player, card):
+        self._cards[player] = card
+
+
+    def taking_player(self, trump_suit):
+
+        taking_card = None
+        taking_player_idx = None
+        for idx in self._ordered_player_indices:
+            played_card = self._cards[self._players[idx]]
+            if played_card.overtakes(taking_card, trump_suit):
+                taking_card = played_card
+                taking_player_idx = idx
+
+        return self._players[taking_player_idx]
+
+
+    def total_points(self, trump_suit):
+        return sum([card.point_value(trump_suit) for card in self.cards])
